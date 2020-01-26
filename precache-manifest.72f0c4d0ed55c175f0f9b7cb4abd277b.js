e967ffcb8e4c945de74bcee92c7ab08fac68f958
QuantumNovice <portabl3lapy@gmail.com>
2020-01-26 20:52:18 +0500

    Build 3

---
+++ precache-manifest.72f0c4d0ed55c175f0f9b7cb4abd277b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4a65e0090676a3dec1dd51f2505ca4d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "731f1ebe69f7f69a7c97",
+    "url": "./static/css/main.453965df.chunk.css"
+  },
+  {
+    "revision": "f1c95c59794dbfe34409",
+    "url": "./static/js/2.61b88120.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "./static/js/2.61b88120.chunk.js.LICENSE"
+  },
+  {
+    "revision": "731f1ebe69f7f69a7c97",
+    "url": "./static/js/main.bdb314fa.chunk.js"
+  },
+  {
+    "revision": "a54a83e1e02d06334baa",
+    "url": "./static/js/runtime-main.130e4e05.js"
+  },
+  {
+    "revision": "7a7beef3bfec49d531a6fa9b789cf506",
+    "url": "./static/media/eye.7a7beef3.png"
+  }
+]);
